6ef8abf2c9b44abe656e9b77d3e9a04384491771
unknown <watsapol.ping@gmail.com>
2024-10-02 16:49:58 +0700

    implement header right side

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Main from "@/components/Main";
+import RightContent from "@/components/RightContent";
 
 export default function Home() {
   return (
     <div className="bg-[#a5a6b3] min-h-screen p-8 flex">
       <Main />
-      {/* RIGHT SIDE */}
-      <div className='bg-[#babbcd]'>
-        Right side
-      </div>
+      <RightContent />
     </div>
   );
 }
---
+++ components/RightContent.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button"
+
+const RightContent = () => {
+  return (
+    <section className="bg-[#f1f2fa] px-12 py-9 rounded-tr-3xl rounded-br-3xl">
+      <div className="container w-[540px]">
+        <Tabs defaultValue="week">
+          <TabsList>
+            <div>
+              <TabsTrigger value="today">Today</TabsTrigger>
+              <TabsTrigger value="week">Week</TabsTrigger>
+            </div>
+            <div className='flex items-center gap-5'>
+              <Button className='rounded-full text-lg'>°C</Button>
+              <Button className='rounded-full text-lg'>°F</Button>
+              <Avatar className='ml-4'>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          </TabsList>
+          <TabsContent value="today">Today</TabsContent>
+          <TabsContent value="week">Week</TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+export default RightContent;
